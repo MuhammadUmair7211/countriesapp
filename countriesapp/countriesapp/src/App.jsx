68c61ba42a15a1a8ac7fc3e46e5862c6f826e20b
Muhammad Umair <muhammadumair7211@gmail.com>
2024-12-24 20:51:14 +0500

    countries data project

--- countriesapp/countriesapp/src/App.jsx
+++ countriesapp/countriesapp/src/App.jsx
@@ -1,27 +1,11 @@
 import "./App.css";
 import Navbar from "./components/Navbar";
-import SearchBar from "./components/SearchBar";
-import SelectMenu from "./components/SelectMenu";
-import CountryList from "./CountryList";
-import { useState } from "react";
+import { Outlet } from "react-router-dom";
 function App() {
-  const [query, setQuery] = useState("");
   return (
     <>
       <Navbar />
-      <div className="container mt-4">
-        <div className="row">
-          <div className="col-md offset-md-1 px-2 py-2">
-            <SearchBar setQuery={setQuery} />
-          </div>
-          <div className="col-md offset-md-1 px-2 py-2">
-            <SelectMenu />
-          </div>
-        </div>
-      </div>
-      <div className="country-cards mt-5">
-        <CountryList query={query} />
-      </div>
+      <Outlet />
     </>
   );
 }
